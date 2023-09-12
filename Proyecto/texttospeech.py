efba064554822cd6a8694ecdceccfe652c8095b2
Juanafenley <111438866+Juanafenley@users.noreply.github.com>
2023-09-12 08:16:30 -0300

    borro todo

--- Proyecto/texttospeech.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-# initialize Text-to-speech engine
-engine = pyttsx3.init() 
-# convert this text to speech
-text = "¿Te caíste?"
-engine.setProperty("rate", 150)
-engine.say(text)
-# play the speech
-engine.runAndWait()
